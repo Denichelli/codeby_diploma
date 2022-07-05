06f8825b02489c5bb1fb01928dbb263c2dca9266
Denichelli <rezenkov@rambler.ru>
2022-07-06 00:32:23 +0300

    -была ошибка в коде, при возврате значения из модуля -еще добавил сам процесс создания директории в исключения

--- diploma/write_in_file.py
+++ diploma/write_in_file.py
@@ -2,19 +2,24 @@ import os
 
 
 def write_in_file(data, extension):
-    folder_path = os.path.join(os.getcwd(), 'images', f'{extension}')
+    folder_path = os.path.join(os.getcwd(), 'images')
+    file_path = os.path.join(os.getcwd(), 'images', f'{extension}')
     if os.path.isdir(folder_path):
-        with open(folder_path, 'wb') as f:
+        with open(file_path, 'wb') as f:
             try:
                 f.write(data)
             except Exception as e:
                 print(e)
                 return False
             else:
-                return True
+                return os.path.getsize(file_path)
     else:
         for i in range(1):
-            os.mkdir('images')
-            write_in_file(data, extension)
+            try:
+                os.mkdir('images')
+            except Exception as e:
+                print(e)
+            else:
+                write_in_file(data, extension)
         print('There is no folder for saving files')
         return False
