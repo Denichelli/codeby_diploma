7ee3695c666e941e9717b760af3cd73d9d775f65
Denichelli <rezenkov@rambler.ru>
2022-07-06 21:31:17 +0300

    -исправил ошибку при получении адреса картинки, которая была перемещена (статус 200 и адрес без протокола "https:") -исправил ошибку при получении имени и расширения картинки (там иногда попадался символ "-" из-за этого патерн не находил совпадений)

--- diploma/host_request.py
+++ diploma/host_request.py
@@ -5,6 +5,23 @@ from colorama import Fore
 from write_in_file import write_in_file as wr_file
 
 
+def fix_link(link):
+    if link.startswith('//'):
+        return link.replace('//', 'https://')
+    else:
+        return link
+
+
+def get_data(link, headers_br):
+    res_link = fix_link(link)
+    try:
+        data = requests.get(res_link, headers=headers_br).content
+    except Exception as e:
+        print(e)
+    else:
+        return data
+
+
 def host_request(address):
     headers_brows = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) "
                                    "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -17,13 +34,15 @@ def host_request(address):
         return False
     else:
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            image_link = soup.find('img', class_="no-click screenshot-image")
-            image_link = image_link.get('src')
-            extension = re.findall(r'.*/(\w+\.\w{3,4}$)', image_link)[0]
             print(f'{Fore.GREEN}GOOD! {Fore.RED}-->{Fore.RESET} {address}')
-            data = requests.get(image_link, headers=headers_brows).content
-            if res := wr_file(data, extension):
+            soup = BeautifulSoup(response.text, 'html.parser')
+            image_link = str(soup.
+                             find('img', class_="no-click screenshot-image").
+                             get('src'))
+            extension = re.findall(r'.*/([\w-]+\.\w{3,4}$)', image_link)
+            print(extension)
+            data = get_data(image_link, headers_brows)
+            if res := wr_file(data, *extension):
                 return [1, res]
         else:
             print(address)
