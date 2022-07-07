faaa8d8976b90a07745d3c95507713b3c4005fde
Denichelli <rezenkov@rambler.ru>
2022-07-07 21:37:14 +0300

    -убрал большинство функций, сделал их модулями и поместил их все в отдельную папку, выделив стартовый файл

--- diploma/diploma_v_1.py
+++ diploma/diploma_v_1.py
@@ -1,15 +1,19 @@
 import pyfiglet
 import requests
 from colorama import Fore
-from choose_method import choose_method as ch_met
-from number_of_threads import select_threads as sel_thr
-from goal_selection import available_target as av_tar
-from selection_of_links import start_pool as st_pool
+from diploma.moduls.choose_method import choose_method
+from diploma.moduls.number_of_threads import select_threads
+from diploma.moduls.goal_selection import available_target
+from diploma.moduls.selection_of_links import convert_args
 
 
 def calling_site(site):
+    headers_brows = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) "
+                                   "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                   "Chrome/72.0.3626.119 Safari/537.36"}
     try:
-        page = requests.get(f'https://{site}', allow_redirects=False)
+        page = requests.get(f'https://{site}/', headers=headers_brows,
+                            timeout=1, allow_redirects=False)
     except Exception as e:
         print(e)
         return False
@@ -26,23 +30,18 @@ def calling_site(site):
 if __name__ == '__main__':
     pyfiglet.print_figlet('My NetStalker', 'big')
 
-    target = av_tar()
-    threads = sel_thr()
-    method = ch_met()
+    target = available_target()
+    threads = select_threads()
+    method = ''
+    for i in (list_methods := choose_method()):
+        if i != 0:
+            method = i
 
     print(f'Target: {Fore.LIGHTBLUE_EX}{target}{Fore.RESET} '
           f'with {Fore.LIGHTBLUE_EX}{threads} threads{Fore.RESET} '
           f'for {Fore.LIGHTBLUE_EX}{method}{Fore.RESET}')
 
     if calling_site(target):
-        print('Next code...')
-        st_pool(target, threads, method)
+        convert_args(target, threads, list_methods)
     else:
         print('EXIT')
-
-# if (sel_ti := sel_ti.lower().replace(' ', '')).endswith('h'):
-#     return int(sel_ti[:-1]) * 3600
-# elif sel_ti.endswith('m'):
-#     return int(sel_ti[:-1]) * 60
-# return int(sel_ti[:-1])
-
