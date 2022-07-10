e79922ba7132b8f70a9df1b7af1410b442ff1d3a
Denichelli <rezenkov@rambler.ru>
2022-07-10 20:45:16 +0300

    довел данный модуль до ума -добавил определение операционной системы и действия, для каждой ОС

--- diploma/moduls/open_folder.py
+++ diploma/moduls/open_folder.py
@@ -1,15 +1,17 @@
 import os
 
 
-def get_os():
-    current_path = os.getcwd()
-    return current_path
+def operation_system():
+    return os.name
 
 
 def open_folder():
-    operating_system = get_os()
-    if answer := (input('Do you want to open a folder with pictures?\n'
-                        '"Y" - open folder with images\n'
-                        '"N" - end program\n'
-                        'Your choice: ')).upper() == 'Y':
-        print('open')
+    target_folder = os.path.join(os.getcwd(), 'images')
+    print(os.path.isdir(target_folder))
+    if input('Do you want to open a folder with pictures?\n'
+             '"Y" - open folder with images\n"N" - end program\n'
+             'Your choice: ').upper() == 'Y':
+        if (op_sys := operation_system()) == 'posix':
+            os.system(f'nautilus {target_folder}')
+        elif op_sys == 'nt':
+            os.system(f'explorer "{target_folder}"')
