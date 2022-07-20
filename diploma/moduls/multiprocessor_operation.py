d5b370b23fa0f10632518220e87a0beb1bef544c
Denichelli <rezenkov@rambler.ru>
2022-07-20 21:02:22 +0300

    -добавил исключение на случай принудительного завершения работы Пуула

--- diploma/moduls/multiprocessor_operation.py
+++ diploma/moduls/multiprocessor_operation.py
@@ -4,5 +4,8 @@ from . import host_request
 
 def pool_operation(req, list_links):
     with Pool(req) as pool:
-        result = pool.map(host_request.address_verification, list_links)
+        try:
+            result = pool.map(host_request.address_verification, list_links)
+        except Exception as e:
+            print(e)
     return result
